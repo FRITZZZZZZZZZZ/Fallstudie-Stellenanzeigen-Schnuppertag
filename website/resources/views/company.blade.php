92221f64a3cfb864c6fed59ed884c4cdee215bc5
Fritz <friedrie07@zedat.fu-berlin.de>
2024-10-20 16:18:08 +0200

    die unterseiten wurden erstellt und sehen zwar sehr öde aber immerhin nicht mehr komplett roh aus. Außerdem wurden bereits die Zusatzaufgaben Breadcrumps und Imige upload erledigt.

--- website/resources/views/company.blade.php
+++ website/resources/views/company.blade.php
@@ -3,16 +3,24 @@
 </head>
 <body class="body">
 <div class="header">
-    <div class="home">
-        <a href="http://127.0.0.1:8000/"><button class="button">Fallstudie</button></a>
+    <div class="navigation">
+        <div class="home">
+            <a href="http://127.0.0.1:8000/"><button class="button">Fallstudie</button></a>
+        </div>
+        <div class="view_select">
+            <a href="http://127.0.0.1:8000/job"><button class="button">Job</button></a>
+            <a href="http://127.0.0.1:8000/company"><button class="button">Company</button></a>
+            <a href="http://127.0.0.1:8000/category"><button class="button">Category</button></a>
+            <a href="http://127.0.0.1:8000/user"><button class="button">User</button></a>
+        </div>
     </div>
-    <div class="view_select">
-        <a href="http://127.0.0.1:8000/job"><button class="button">Job</button></a>
-        <a href="http://127.0.0.1:8000/company"><button class="button">Company</button></a>
-        <a href="http://127.0.0.1:8000/category"><button class="button">Category</button></a>
-        <a href="http://127.0.0.1:8000/user"><button class="button">User</button></a>
+    <div class="breadcrumps">
+        <div class="breadcrump_env">
+            <a class="breadcrump_link" href="http://127.0.0.1:8000/">Home > </a><a class="breadcrump_link">Company</a>
+        </div>
     </div>
 </div>
+
 <div class="main">Test</div>
-<div class="footer">Footer</div>
+<div class="footer"><a class="breadcrump_link" href="https://github.com/FRITZZZZZZZZZZ/Fallstudie-Stellenanzeigen-Schnuppertag">Dieses Projekt auf GitHub</a></div>
 </body>
--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -3,16 +3,30 @@
 </head>
 <body class="body">
 <div class="header">
-    <div class="home">
-        <a href="http://127.0.0.1:8000/"><button class="button">Fallstudie</button></a>
+    <div class="navigation">
+        <div class="home">
+            <a href="http://127.0.0.1:8000/"><button class="button">Fallstudie</button></a>
+        </div>
+        <div class="view_select">
+            <a href="http://127.0.0.1:8000/job"><button class="button">Job</button></a>
+            <a href="http://127.0.0.1:8000/company"><button class="button">Company</button></a>
+            <a href="http://127.0.0.1:8000/category"><button class="button">Category</button></a>
+            <a href="http://127.0.0.1:8000/user"><button class="button">User</button></a>
+        </div>
     </div>
-    <div class="view_select">
-        <a href="http://127.0.0.1:8000/job"><button class="button">Job</button></a>
-        <a href="http://127.0.0.1:8000/company"><button class="button">Company</button></a>
-        <a href="http://127.0.0.1:8000/category"><button class="button">Category</button></a>
-        <a href="http://127.0.0.1:8000/user"><button class="button">User</button></a>
+    <div class="breadcrumps">
+        <div class="breadcrump_env">
+            <a class="breadcrump_link">Home</a>
+        </div>
     </div>
 </div>
-<div class="main">Test</div>
-<div class="footer">Footer</div>
+
+<div class="main">
+    <div class="text_env">
+        <h1 class="heading_1">Fallstudie Stellenanzeigen</h1>
+        <p class="text_left">Diese Website ist das Ergenis aus den Aufgaben des Folgenden Arbeitsblattes:</p>
+    </div>
+    <img class="images" src="\images\Fallstudien_Arbeitsblatt.png">
+</div>
+<div class="footer"><a class="breadcrump_link" href="https://github.com/FRITZZZZZZZZZZ/Fallstudie-Stellenanzeigen-Schnuppertag">Dieses Projekt auf GitHub</a></div>
 </body>
